e0fac9d7326f8b73165dcc6952c46c6705681709
Imran Khan <khalekuzzamanimran@gmail.com>
2023-12-26 23:49:02 +0600

    add-shadcn-btn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
+import { Routes, Route } from "react-router-dom";
 import "./globals.css";
+import SignInForm from "./_auth/forms/SignInForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main className="flex h-screen">
+      <Routes>
+        {/* Public route */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+
+        {/* Private route */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
